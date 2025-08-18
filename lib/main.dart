03e0d6bd3e5e995789f3662f2773142812e57da0
AlwaleedKhalidAltrimi <alwaleedaltrimi@gmail.com>
2025-08-18 10:29:41 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdaptiveLayoutPractice());
+}
+
+class AdaptiveLayoutPractice extends StatelessWidget {
+  const AdaptiveLayoutPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
